ba58e1d0fb4932f4dd1447dce8972957a6da0b56
Ivan Chavarro <ivan.chavarro@globant.com>
2024-05-20 21:02:46 -0500

    Fixing instalations errors.

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,23 +1,22 @@
-const serverless = require('serverless-http');
-const { createServer, proxy } = require('aws-serverless-express');
+const serverless = require('aws-serverless-express');
 const express = require('express');
-const { AppModule } = require('../dist/app.module');
 const { NestFactory } = require('@nestjs/core');
+const { ExpressAdapter } = require('@nestjs/platform-express');
 
 let cachedServer;
 
 async function bootstrapServer() {
   if (!cachedServer) {
     const expressApp = express();
+    const { AppModule } = require('./dist/app.module');
     const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
     await nestApp.init();
-    cachedServer = createServer(expressApp);
+    cachedServer = serverless.createServer(expressApp);
   }
   return cachedServer;
 }
 
 module.exports.handler = async (event, context) => {
   const server = await bootstrapServer();
-  return proxy(server, event, context, 'PROMISE').promise;
+  return serverless.proxy(server, event, context, 'PROMISE').promise;
 };
-
